c6b064fe6db6e7ee20fcf4066facb4ba8dab0366
RELIAS\jgrimes <jgrimes@reliaslearning.com>
2018-04-04 15:53:52 -0400

    Add table to Users page

--- benefits-tool/src/app/shared/custom-material-module.module.ts
+++ benefits-tool/src/app/shared/custom-material-module.module.ts
@@ -1,8 +1,8 @@
-import { MatButtonModule, MatSidenavModule } from '@angular/material';
+import { MatButtonModule, MatSidenavModule, MatTableModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [ MatButtonModule, MatSidenavModule ],
-  exports: [ MatButtonModule, MatSidenavModule ],
+  imports: [ MatButtonModule, MatSidenavModule, MatTableModule ],
+  exports: [ MatButtonModule, MatSidenavModule, MatTableModule ],
 })
 export class CustomMaterialModule { }
