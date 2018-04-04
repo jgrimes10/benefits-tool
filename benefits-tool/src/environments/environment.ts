fa7fac566445e8c2a1cfc21dac3d16aae66451aa
Lindsey Adams <linzma87@gmail.com>
2018-04-04 17:54:33 -0400

    remove key

--- benefits-tool/src/environments/environment.ts
+++ benefits-tool/src/environments/environment.ts
@@ -3,14 +3,3 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyAPAH4vOj5VrX5EUld4Kk4HUqGsLu9vyow',
-    authDomain: 'benefits-tool.firebaseapp.com',
-    databaseURL: 'https://benefits-tool.firebaseio.com',
-    projectId: 'benefits-tool',
-    storageBucket: 'benefits-tool.appspot.com',
-    messagingSenderId: '206660767572'
-  }
-};
