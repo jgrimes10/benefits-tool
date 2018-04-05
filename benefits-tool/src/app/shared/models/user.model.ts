264a9195fd6cc6861075c98d3c4d0aceebeed329
Lindsey Adams <linzma87@gmail.com>
2018-04-05 09:41:49 -0400

    Work on user form

--- benefits-tool/src/app/shared/models/user.model.ts
+++ benefits-tool/src/app/shared/models/user.model.ts
@@ -1,13 +1,14 @@
 export class User {
 
-  constructor(email, isAdmin, firstName?, lastName?, position?, salary?, bonus?,
+  constructor(email?, isAdmin?, firstName?, lastName?, position?, department?, salary?, bonus?,
     _401k?, medical?, dental?, hsa?, pto?, numberOfPtoDaysTaken?, tuition?
   ) {
-    this.isAdmin = isAdmin;
-    this.email = email;
+    this.isAdmin = isAdmin || null;
+    this.email = email || null;
     this.firstName = firstName || null;
     this.lastName = lastName || null;
     this.position = position || null;
+    this.department = department || null;
     this.salary = salary || null;
     this.bonus = bonus || null;
     this._401k = _401k || null;
@@ -24,6 +25,7 @@ export class User {
   firstName: string;
   lastName: string;
   position: string;
+  department: string;
   salary: number;
   bonus: number;
   _401k: number;
--- benefits-tool/src/app/users/create-user/create-user.component.ts
+++ benefits-tool/src/app/users/create-user/create-user.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { User } from '../../shared/models/user.model';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-create-user',
@@ -7,10 +11,30 @@ import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent implements OnInit {
+  user: User;
+  userForm: FormGroup;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  buildForm() {
+    this.user = this.user || new User();
+    this.userForm = this.formBuilder.group({
+      firstName: [this.user.firstName],
+      lastName: [this.user.lastName],
+      email: [this.user.email],
+      department: [this.user.department],
+      position: [this.user.position],
+      salary: [this.user.salary],
+      admin: [this.user.isAdmin]
+    });
+  }
+
+  createUser() {
+    this.userService.createUser(this.user).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
