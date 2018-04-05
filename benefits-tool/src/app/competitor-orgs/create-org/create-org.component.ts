6e69cab85f3a6db61184af6ee25f0d731b9340c7
RELIAS\jgrimes <jgrimes@reliaslearning.com>
2018-04-05 14:31:50 -0400

    Finish creating org logic

--- benefits-tool/src/app/competitor-orgs/create-org/create-org.component.ts
+++ benefits-tool/src/app/competitor-orgs/create-org/create-org.component.ts
@@ -3,6 +3,9 @@ import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FormGroup, FormControl, FormGroupDirective, NgForm, Validators, FormBuilder } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
+import { CompetitorOrganization } from '../../shared/models/organization.model';
+import { OrganizationService } from '../../shared/services/organization.service';
+
 @Component({
   selector: 'app-create-org',
   templateUrl: './create-org.component.html',
@@ -10,9 +13,54 @@ import { Observable } from 'rxjs/Observable';
 })
 export class CreateOrgComponent implements OnInit {
 
-  constructor() { }
+  competitor: CompetitorOrganization;
+  orgForm: FormGroup;
+
+  constructor(
+    public diaglogRef: MatDialogRef<CreateOrgComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private formBuilder: FormBuilder,
+    private orgService: OrganizationService
+  ) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.orgForm = this.formBuilder.group({
+      'orgName': ['', Validators.required],
+      'avgSalary': [''],
+      'avgBonus': [''],
+      'avgPTO': [''],
+      'avgTuition': [''],
+      'avgMedical': [''],
+      'avg401k': [''],
+      'avgDental': [''],
+      'avgVision': ['']
+    });
+  }
+
+  onSave() {
+    const form = this.orgForm;
+    const org = new CompetitorOrganization(
+      form.controls.orgName.value,
+      form.controls.avgSalary.value,
+      form.controls.avgBonus.value,
+      form.controls.avg401k.value,
+      form.controls.avgMedical.value,
+      form.controls.avgDental.value,
+      form.controls.avgVision.value,
+      form.controls.avgTuition.value,
+      form.controls.avgPTO.value,
+    );
+
+    this.orgService.insertCompetitorOrganization(org);
+    this.diaglogRef.close();
+  }
+
+  onCancel() {
+    this.diaglogRef.close();
   }
 
 }
--- benefits-tool/src/app/shared/models/organization.model.ts
+++ benefits-tool/src/app/shared/models/organization.model.ts
@@ -1,4 +1,17 @@
 export class CompetitorOrganization {
+
+  constructor(name, avgSalary?, avgBonus?, avg401k?, avgMedical?, avgDental?, avgVision?, avgTuition?, avgPTO?) {
+    this.name = name;
+    this.avgSalary = avgSalary || 0;
+    this.avgBonus = avgBonus || 0;
+    this.avg401k = avg401k || 0;
+    this.avgMedical = avgMedical || 0;
+    this.avgDental = avgDental || 0;
+    this.avgVision = avgVision || 0;
+    this.avgTuition = avgTuition || 0;
+    this.avgPTO = avgPTO || 0;
+  }
+
   name: string;
   avgSalary: number;
   avgBonus: number;
