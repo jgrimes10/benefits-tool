6350e4a07a7c9fbd13f4b768140e0c088a656f12
Eugene Kwok <ekwok@reliaslearning.com>
2018-04-05 18:23:39 -0400

    push test success

--- benefits-tool/src/app/shared/services/xlsx.service.ts
+++ benefits-tool/src/app/shared/services/xlsx.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import * as XLSX from 'xlsx';
 
+import { OrganizationService } from './organization.service';
+import { CompetitorOrganization } from '../models/organization.model';
+
 @Injectable()
 export class XlsxService {
 
-  constructor() { }
+  constructor(private orgService: OrganizationService) { }
 
   uploadFile(file: File) {
     const reader = new FileReader();
@@ -16,7 +19,13 @@ export class XlsxService {
       const ws = wb.Sheets[wsname];
 
       const data = (XLSX.utils.sheet_to_json(ws, {header: 1}));
-      console.log(data);
+      console.log(data[1]);
+      const firstRow = data[1];
+
+      const pushTest = new CompetitorOrganization(firstRow[0].trim(), firstRow[1].trim(), Number(firstRow[2].trim()),
+        Number(firstRow[3].trim()), Number(firstRow[5].trim()), Number(firstRow[9].trim()), Number(firstRow[10].trim()),
+        Number(firstRow[11].trim()), Number(firstRow[6].trim()), Number(firstRow[8].trim()));
+      this.orgService.insertCompetitorOrganization(pushTest);
     };
     reader.readAsBinaryString(file);
   }
