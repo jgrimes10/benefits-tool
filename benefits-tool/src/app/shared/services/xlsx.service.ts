19eb9964e5a9271f55b3c07948d04cd75b7129cd
Eugene Kwok <ekwok@reliaslearning.com>
2018-04-05 17:54:50 -0400

    xlsx console log data

--- benefits-tool/src/app/shared/services/xlsx.service.ts
+++ benefits-tool/src/app/shared/services/xlsx.service.ts
@@ -6,4 +6,18 @@ export class XlsxService {
 
   constructor() { }
 
+  uploadFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr = e.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+
+      const data = (XLSX.utils.sheet_to_json(ws, {header: 1}));
+      console.log(data);
+    };
+    reader.readAsBinaryString(file);
+  }
 }
